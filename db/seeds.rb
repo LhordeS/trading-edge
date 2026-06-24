5a8bd8425e7fbbd522ff497758f32d78fbe088e4
Alain Mimeault <lhorde@msn.com>
2026-06-24 19:56:19 +0900

    Refactor seed file to destroy existing db entries and to be more DRY

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,49 @@
-SetupDefinition.find_or_create_by!(name: "opening Range Breakout") do |s|
-  s.version = 1.0
-  s.active = true
+puts "Clearing existing seed data..."
+
+MarketEvent.destroy_all
+TradingDay.destroy_all
+SetupDefinition.destroy_all
+
+puts "Creating setup definitions..."
+
+# SetupDefinition.find_or_create_by!(name: "opening Range Breakout") do |s|
+#   s.version = 1.0
+#   s.active = true
+# end
+
+# SetupDefinition.find_or_create_by!(name: "VWAP Reclaim") do |s|
+#   s.version = 1.0
+#   s.active = true
+# end
+
+# SetupDefinition.find_or_create_by!(name: "Trend Pullback") do |s|
+#   s.version = 1.0
+#   s.active = true
+# end
+
+setup_definitions = [
+  {
+    name: "Opening Range Breakout",
+    version: 1.0,
+    active: true
+  },
+  {
+    name: "VWAP Reclaim",
+    version: 1.0,
+    active: true
+  },
+  {
+    name: "Trend Pullback",
+    version: 1.0,
+    active: true
+  }
+]
+
+setup_definitions.each do |attrs|
+  SetupDefinition.find_or_create_by!(name: attrs[:name]) do |setup|
+    setup.version = attrs[:version]
+    setup.active = attrs[:active]
+  end
 end
 
-SetupDefinition.find_or_create_by!(name: "VWAP Reclaim") do |s|
-  s.version = 1.0
-  s.active = true
-end
-
-SetupDefinition.find_or_create_by!(name: "Trend Pullback") do |s|
-  s.version = 1.0
-  s.active = true
-end
+puts "Setup definitions successfully created"
